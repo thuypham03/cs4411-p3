adf4198f54a775a3a599fa4a428dcea4a5a86c4c
thuypham03 <sophia.phamthuy@gmail.com>
2024-03-21 23:38:33 -0400

    implement wtclockdisk

--- src/block/wtclockdisk.c
+++ src/block/wtclockdisk.c
@@ -8,6 +8,20 @@
 #include <string.h>
 #include <egos/block_store.h>
 
+enum block_status {
+	EMPTY,	// block not in use
+	OLD,	// block not used for a while (ready to be replaced)
+	NEW		// block recently used
+};
+
+/* Metadata for a single cache slot, including address of underlying
+ * storage (if in use), empty otherwise
+ */
+typedef struct block_info {
+	enum block_status status;
+	unsigned int ino;
+	block_no offset;
+} block_info_t;
 
 /* State contains the pointer to the block module below as well as caching
  * information and caching statistics.
@@ -15,13 +29,14 @@
 struct wtclockdisk_state {
 	block_if below;				// block store below
 	block_t *blocks;			// memory for caching blocks
+	block_info_t *block_infos;  // memory for caching blocks' metadata
 	block_no nblocks;			// size of cache (not size of block store!)
+	block_no clock_hand;
 
 	/* Stats.
 	 */
 	unsigned int read_hit, read_miss, write_hit, write_miss;
 };
-
 
 static int wtclockdisk_getninodes(block_store_t *this_bs){
 	struct wtclockdisk_state *cs = this_bs->state;
@@ -35,30 +50,88 @@ static int wtclockdisk_getsize(block_if bi, unsigned int ino){
 
 static int wtclockdisk_setsize(block_if bi, unsigned int ino, block_no nblocks){
 	struct wtclockdisk_state *cs = bi->state;
+	
+	// Clear cache's entries of about-to-be-deleted blocks
+	for (block_no i = 0; i < cs->nblocks; ++i) {
+		if (cs->block_infos[i].status != EMPTY && 
+			cs->block_infos[i].ino == ino && cs->block_infos[i].offset >= nblocks) {
+				cs->block_infos[i].status = EMPTY;
+			}
+	}
 
 	return (*cs->below->setsize)(cs->below, ino, nblocks);
 }
 
+static void cache_update(struct wtclockdisk_state *cs, unsigned int ino, block_no offset, block_t *block) {
+	while (1) {
+		block_no id = cs->clock_hand;
+		if (cs->block_infos[id].status != NEW) {
+			// Evict this cache slot
+			cs->block_infos[id].status = NEW;
+			cs->block_infos[id].ino = ino;
+			cs->block_infos[id].offset = offset;
+			memcpy(&cs->blocks[id], block, sizeof(block_t));
+
+			cs->clock_hand = (id + 1) % cs->nblocks;
+			return;
+		} else 
+			cs->block_infos[id].status = OLD;
+
+		cs->clock_hand = (id + 1) % cs->nblocks;
+	}
+}
+
 static int wtclockdisk_read(block_if bi, unsigned int ino, block_no offset, block_t *block){
-	/* Your code should replace this naive implementation
-	 */
 	struct wtclockdisk_state *cs = bi->state;
 
+	for (block_no i = 0; i < cs->nblocks; ++i) {
+		if (cs->block_infos[i].status != EMPTY && 
+			cs->block_infos[i].ino == ino && cs->block_infos[i].offset == offset) {
+				// Cache hit
+				memcpy(block, &cs->blocks[i], sizeof(block_t));
+				cs->block_infos[i].status = NEW;
+				cs->read_hit += 1;
+				return 0;
+			}
+	}
+
+	// Cache miss
+	cs->read_miss += 1;
+
 	int r = (*cs->below->read)(cs->below, ino, offset, block);
-	return r;
+	if (r == -1) return r;
+
+	cache_update(cs, ino, offset, block);
+	return 0;
 }
 
 static int wtclockdisk_write(block_if bi, unsigned int ino, block_no offset, block_t *block){
-	/* Your code should replace this naive implementation
-	 */
-	
 	struct wtclockdisk_state *cs = bi->state;
 
-	return (*cs->below->write)(cs->below, ino, offset, block);
+	for (block_no id = 0; id < cs->nblocks; ++id) {
+		if (cs->block_infos[id].status != EMPTY && 
+			cs->block_infos[id].ino == ino && cs->block_infos[id].offset == offset) {
+				// Cache hit
+				memcpy(&cs->blocks[id], block, sizeof(block_t));
+				cs->block_infos[id].status = NEW;
+				cs->write_hit += 1;
+				return (*cs->below->write)(cs->below, ino, offset, block);
+			}
+	}
+
+	// Cache miss
+	cs->write_miss += 1;
+
+	int w = (*cs->below->write)(cs->below, ino, offset, block);
+	if (w == -1) return w;
+
+	cache_update(cs, ino, offset, block);
+	return 0;
 }
 
 static void wtclockdisk_release(block_if bi){
 	struct wtclockdisk_state *cs = bi->state;
+	free(cs->block_infos);
 	free(cs);
 	free(bi);
 }
@@ -88,6 +161,9 @@ block_if wtclockdisk_init(block_if below, block_t *blocks, block_no nblocks){
 	cs->below = below;
 	cs->blocks = blocks;
 	cs->nblocks = nblocks;
+	cs->clock_hand = 0;
+	cs->block_infos = calloc(nblocks, sizeof(*cs->block_infos));
+
 	cs->read_hit = 0;
 	cs->read_miss = 0;
 	cs->write_hit = 0;
